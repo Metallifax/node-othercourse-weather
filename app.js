f474de4f2e2bd641f4ee4690fb0a42e79976df06
Metallifax <61431322+Metallifax@users.noreply.github.com>
2020-10-16 10:19:31 -0300

    Update app.js
    Hidden API key

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const request = require('request');
-const url = 'http://api.weatherstack.com/current?access_key=79a1d0825b61347927e5a8dc99a40563&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=79a**************************&query=37.8267,-122.4233'
 
 request({url: url}, (err, res) => {
   const data = JSON.parse(res.body);

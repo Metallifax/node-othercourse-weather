bfa3fd0ea96d10b0a0ef4a742fc88f492da2484d
Metallifax <61431322+Metallifax@users.noreply.github.com>
2020-10-18 16:13:59 -0300

    made the app a little more consistent with variables

--- utils/forecast.js
+++ utils/forecast.js
@@ -1,9 +1,12 @@
 const request = require('request');
 const process = require('process');
+
 require('dotenv').config();
 
 const forecast = (lat, long, callback) => {
-  request({ url: `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API_KEY}&query=${lat},${long}&units=m`, json: true}, (err, res) => {
+  const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API_KEY}&query=${lat},${long}&units=m`;
+
+  request({ url: url, json: true}, (err, res) => {
     if(err){
       callback('Unable to connect to weather services.', undefined);
     } else if (res.body.message) {
